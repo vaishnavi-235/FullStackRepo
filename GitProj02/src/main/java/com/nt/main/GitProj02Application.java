a85368b25d9264f3e86a7b2e001645e4ebfc7344
onkar <onkar@hp>
2025-08-09 17:11:30 +0530

    commit

---
+++ GitProj02/src/main/java/com/nt/main/GitProj02Application.java
@@ -0,0 +1,13 @@
+package com.nt.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProj02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProj02Application.class, args);
+	}
+
+}
---
+++ GitProj02/src/test/java/com/nt/main/GitProj02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitProj02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
